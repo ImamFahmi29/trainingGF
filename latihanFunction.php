a026ac116d2fa305b8342998b1db0c339e9ad8ae
IMAM FAHMI FADILLAH <imam.fahmi29@gmail.com>
2017-11-07 13:03:03 +0700

    Revisi Fix

--- latihanFunction.php
+++ latihanFunction.php
@@ -14,24 +14,31 @@
 		?>
 		<h3>No.2 Function</h3>
 		<?php
-			$split="12 08 31";
-			echo "<pre>";
-			print_r(explode(" ", $split,3));
-			echo "</pre>";
+			function pisah(){
+				$angka="120831";
+				for ($i=0; $i < strlen($angka) ; $i++) {
+					if ($i%2==0) {
+						echo " ";
+					}echo $angka[$i];
+				}
+			}
+			pisah();
 			?>
 		<h3>No.3 Function</h3>
 		<?php
-			function hurufTengah($key){
-			$wlen = strlen($key);
-   			$lPad = floor($wlen/2)+1;
-    		$rPad = ceil($wlen/2);
-   			$left = substr($key, 0, -$lPad);
-   			$middle = substr($key, -$lPad, 1); 
-   			$right = substr($key, $rPad, $lPad);
-   			return $middle;
+			function hurufTengah(){
+			$key="IMAM FAHMI";
+			$tengah=strlen($key)/2;
+			$atas=ceil($tengah);
+			$bwh=floor($tengah)-1;
+			echo "Input : ".$key."<br><br>";
+			if(strlen($key)%2==0){
+				echo "Hasilnya dari kata ".$key." adalah huruf : <b>".substr($key,$bwh,2)."</b><br>";
+			}else{
+				echo "Hasilnya dari kata ".$key." adalah huruf : <b>".substr($key,$tengah,1)."</b><br>";
+				}
    			}
-   			echo "huruf tengah dari kata FAHMI adalah : <b>" .hurufTengah("FAHMI")."</b>";
-			
+   			hurufTengah();		
 			?>
 		<h3>No.4 Function</h3>
 		<?php
@@ -41,20 +48,37 @@
 		?>
 		<h3>No.5 Function</h3>
 		<?php
-			function hitungKata ($input){
-			// $input  = "The quick brown fox jumps over the lazy dog"; 
+			function hitungKata (){
+			$input  = "The quick brown fox jumps over the lazy dog"; 
 			$input = str_word_count($input);
 			return $input;
 			}
-			echo "Number of words in the string : ".hitungKata("The quick brown fox jumps over the lazy dog");
+			echo "Number of words in the string : ".hitungKata();
 		?>
 		<h3>No.6 Function</h3>
 		<?php
-		function figureValue($principal,$rate,$years){
-   		$futureValue = round($principal * pow(1 + ($rate/100), $years), 2);
-   		return $futureValue;
-    	}
-    	echo figureValue(1000,10,5);
+	function futureInvestmentValue($amount, $InterestRate,$years) {
+		return $amount * pow(1 + ($InterestRate/12), $years * 12);
+	}
+		$amount=1000;
+		$InterestRate=10;
+		$years=5;
+		$InterestRate *= 0.01;
+		echo "Years &nbsp;&nbsp;&nbsp;Future Value<br>";
+		for ($i=1; $i <= $years  ; $i++) { 
+			echo $i."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".futureInvestmentValue($amount,$InterestRate,$i)."<br>";
+		}
    		?>
+   		<H3>No.7 Function</H3>
+   		<?php
+   		function karakter($awal,$akhir,$limit){
+   			for ($i=1; $awal <= $akhir ; $i++,$awal++) {
+   				echo chr($awal)." ";
+   				if ($i%$limit==0) {
+   					echo "<br>";
+   				 } 
+   			}
+   	   	}echo karakter(ord('('),ord('z'),20);
+    ?>
 </body>
 </html>
